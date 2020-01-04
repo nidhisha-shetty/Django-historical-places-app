5bb1e7d34f216e583ea1e07544abcdb0a017bd3b
nidhisha-shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-01-04 18:43:35 +0530

    added base.html file

--- places/views.py
+++ places/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+		#Create your views here.
+
+# def home_view(request, *args, **kwargs):						#similarlly we can use "def home_view(request):"
+# 	print(args, kwargs)
+# 	return HttpResponse("<H1> MY HISTORICAL PLACES PAGE</H1>") #requires "from django.http import HttpResponse"
+
+
+def home_view(request):
+	return render(request, "home.html", {})
+
