0a1cfaa416472c0a22caa9b26a3583045d2bb12b
nidhisha-shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-03-31 13:11:45 +0530

    database fields setup

--- places/models.py
+++ places/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Places(models.Model):
+	place_name = models.TextField(max_length=100, default="Please add name");
+	place_ratings = models.IntegerField(null=True, default=2)
+	place_image = models.ImageField(upload_to="images/", default="images/Varanasi-River-Gange")
+	place_description = models.TextField(max_length=50, default="Please add description")
