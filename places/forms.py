a1f89dd81017ce691dd12f459e98790492912861
nidhisha-shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-05-18 23:53:28 +0530

    adding placeholders to the fields

--- places/forms.py
+++ places/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import Places
 
 class Form(forms.ModelForm):
+	place_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Add name of the place"}))
+	place_ratings = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Add ratings to the place"}))
+	place_description = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Add description of the place"}))
+
 	class Meta:
 		model = Places
 		fields = [
