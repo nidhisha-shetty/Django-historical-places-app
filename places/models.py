52a1e91fc0b64b5facfae25074c303980fcfbcb7
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-06-23 00:46:58 +0530

    add extension to image

--- places/models.py
+++ places/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Places(models.Model):
 	place_name = models.CharField(max_length=100, default="");
 	place_ratings = models.IntegerField(null=True, default="")
-	place_image = models.ImageField(upload_to="images/", default="images/Varanasi-River-Gange")
+	place_image = models.ImageField(upload_to="images/", default="images/Varanasi-River-Gange.jpg")
 	place_description = models.TextField(max_length=50, default="")
 
 	def get_absolute_url_read(self):
